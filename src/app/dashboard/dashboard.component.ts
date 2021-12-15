b9f16a4c3513c8b5b078cad434e7410c2381ae28
kibet <kmishmael@gmail.com>
2021-12-15 11:34:54 +0300

    Post Service added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PostService } from '../post.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private postService: PostService){ }
 
   ngOnInit(): void {
   }
