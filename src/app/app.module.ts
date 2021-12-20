9794bcbb10823e707ed0010179b33aeb3822833c
kibet <kmishmael@gmail.com>
2021-12-21 01:15:46 +0300

    form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TechnologyComponent } from './tagposts/tagposts.component';
@@ -30,7 +30,8 @@ import { PosteditComponent } from './postedit/postedit.component';
     FormsModule,
     NgxPaginationModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    ReactiveFormsModule
   ],
   providers: [
     {
--- src/app/postedit/postedit.component.ts
+++ src/app/postedit/postedit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-postedit',
@@ -10,6 +11,18 @@ export class PosteditComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  formEdit = new FormGroup({
+    title: new FormControl(''),
+    author: new FormControl(''),
+    date: new FormControl(''),
+    content: new FormControl(''),
+    category: new FormControl('')
+  });
+
+  onSubmit(): void{
+    console.log(this.formEdit.value);
   }
 
 }
